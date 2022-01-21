4ba39539266e074307a5dcee5dfd74b46d4ba3e4
nesrin-elasiry <nesrina.m810@gmail.com>
2022-01-21 22:06:18 +0200

    Reflect pull request comments
    I added sidebar as common component between login/signup pages, i handled all styling using makestyle material ui

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
-import bubble from './images/bubble.svg';
-import background from './images/bg-img.png';
+import  SideBar  from "./SideBar";
 import { makeStyles } from "@material-ui/core/styles";
 
 import {
@@ -25,51 +24,6 @@ const useStyles = makeStyles(() => ({
     width: '100%',
     overflow: 'auto',
     height: '100%'
-  },
-  sideBar: {
-    width: '40%',
-    float: 'left',
-    position: 'relative',
-    backgroundImage: 'url('+background+')',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    '@media screen and (max-width: 481px)' : {
-      width: '100%',
-      height: '40%',
-    }
-  },
-  background: {
-    background: 'linear-gradient(180deg, #3A8DFF 0%, #86B9FF 100%)',
-    opacity: 0.85,
-    height: '100%',
-  },
-  middleText: {
-    position: 'absolute',
-    left: '57px',
-    right: '57px',
-    margin: '0px auto',
-    top: '40%',
-    bottom: '40%',
-    textAlign: 'center',
-    display: 'grid',
-    '& img': {
-      position: 'relative',
-      left: '37%',
-      bottom: '20px',
-    },
-    '& p': {
-      position: 'relative',
-      top: '0px',
-      fontWeight: 'normal',
-      fontSize: '26px',
-      textAlign: 'center',
-      color: '#FFFFFF',
-      fontFamily: 'Open Sans',
-      lineHeight: '40px',
-      '@media screen and (max-width: 481px)' : {
-        fontSize: '20px',
-      }
-    }
   },
   column: {
     padding: '10px 40px',
@@ -164,14 +118,7 @@ const Login = (props) => {
 
   return (
     <Grid container className={classes.root}>
-      <Grid item className={classes.sideBar}>
-        <Box component="div" className={classes.background}>
-          <Box component="div" className={classes.middleText}>
-            <Box component="img" src={bubble}/>
-            <Typography>Converse with anyone with any language</Typography>
-          </Box>
-        </Box>
-      </Grid>
+      <SideBar/>
       <Grid item className={classes.column}>
           <Box>
               <Grid container justify="right" item className={classes.topRightPanel}>
@@ -182,7 +129,7 @@ const Login = (props) => {
                 <form onSubmit={handleLogin}>
                   <Typography component="h1">Welcome back!</Typography>
                   <Grid item xs={9}>
-                    <FormControl margin="normal" style={{width: '100%'}} required>
+                    <FormControl margin="normal" style={{width: '100%', marginBottom: '30px'}} required>
                       <TextField
                         aria-label="username"
                         label="Username"
