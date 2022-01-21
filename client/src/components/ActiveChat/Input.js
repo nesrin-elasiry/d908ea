98d17732f01165f386f106c6846949fe7d291caa
nesrin-elasiry <nesrina.m810@gmail.com>
2022-01-21 02:37:46 +0200

    Update Input.js

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -9,7 +9,6 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { Box, Typography, Button } from '@material-ui/core';
 import InputAdornment from '@material-ui/core/InputAdornment';
